d488fc7f5c9d7feea9420859ca845e6ac0a989ff
Thomas E <raymondcy1207@gmail.com>
2024-01-27 17:48:48 -0800

    data_preparation

---
+++ src/main/java/com/your/project/MyVersion.java
@@ -0,0 +1,19 @@
+package com.your.project;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class MyVersion {
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "2024-FRC-Testbed";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 199;
+	public static final String GIT_SHA = "fd1b932055877a45d1b34f4486ee21dceb4be79d";
+	public static final String GIT_DATE = "2024-01-27T23:15:37Z";
+	public static final String GIT_BRANCH = "DataAnalysis";
+	public static final String BUILD_DATE = "2024-01-28T00:20:44Z";
+	public static final long BUILD_UNIX_TIME = 1706401244710L;
+	public static final int DIRTY = 1;
+
+	private MyVersion(){}
+}
