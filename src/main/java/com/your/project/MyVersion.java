64f5a9a14f8afddb01b60f15f7a1a62c8ae5cb8c
RAYYY <56778123+raymond-cy@users.noreply.github.com>
2024-02-15 18:00:18 -0800

    update the version of graddle file

--- src/main/java/com/your/project/MyVersion.java
+++ src/main/java/com/your/project/MyVersion.java
@@ -7,12 +7,12 @@ public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2024-FRC-Testbed";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 199;
-	public static final String GIT_SHA = "fd1b932055877a45d1b34f4486ee21dceb4be79d";
-	public static final String GIT_DATE = "2024-01-27T23:15:37Z";
+	public static final int GIT_REVISION = 200;
+	public static final String GIT_SHA = "d488fc7f5c9d7feea9420859ca845e6ac0a989ff";
+	public static final String GIT_DATE = "2024-01-28T01:48:48Z";
 	public static final String GIT_BRANCH = "DataAnalysis";
-	public static final String BUILD_DATE = "2024-01-28T00:20:44Z";
-	public static final long BUILD_UNIX_TIME = 1706401244710L;
+	public static final String BUILD_DATE = "2024-02-16T01:53:14Z";
+	public static final long BUILD_UNIX_TIME = 1708048394752L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
